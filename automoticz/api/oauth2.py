d59b5cc351960ddcdd8e79d396d29b703ad774bc
xbound <xbound@example.com>
2019-02-21 22:37:58 +0100

    Updated dependencies

--- automoticz/api/oauth2.py
+++ automoticz/api/oauth2.py
@@ -1,1 +1,29 @@
+from flask import current_app as app
+from flask import session
+from flask import redirect
+from google.oauth2 import credentials
+from google_auth_oauthlib import flow 
+from googleapiclient import discovery
+
 from automoticz.api.views import oauth2_blueprint
+
+API_NAME = 'proximitybeacon'
+API_VERSION = 'v1beta1'
+SCOPES = ['https://www.googleapis.com/auth/userlocation.beacon.registry']
+
+@oauth2_blueprint.route('/authorize')
+def authorize():
+    auth_flow = flow.Flow.from_client_secrets_file(
+      app.config.CLIENT_SECRETS_FILE, scopes=SCOPES)
+    authorization_url, state = auth_flow.authorization_url(
+      access_type='offline',
+      include_granted_scopes='true')
+    
+    auth_flow.redirect_uri = app.config.REDIRECT_URL
+    session['state'] = state
+    return redirect(authorization_url)
+
+@oauth2_blueprint.route('/callback')
+def oauth2callback():
+    pass
+

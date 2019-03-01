35e6afef0a8be9f08453fabbbc5b9a05dbeed6f1
xbound <xbound@example.com>
2019-03-01 07:01:30 +0100

    Added exception handler for google.auth.RefreshError.

--- automoticz/api/views.py
+++ automoticz/api/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask_restplus import Api
+from google.auth import exceptions
 
+from automoticz.utils.constants import MESSAGE
 from automoticz.api.endpoints import *
 
 oauth2_blueprint = Blueprint('oauth2', __name__, url_prefix='/oauth2')
@@ -18,3 +20,8 @@ api = Api(
 api.add_namespace(ping_namespace, path='/ping')
 api.add_namespace(auth_namespace, path='/auth')
 api.add_namespace(activate_namespace, path='/activate')
+
+# Handling errors
+@api.errorhandler(exceptions.RefreshError)
+def proximity_api_is_not_initialized(error):
+    return {'message': MESSAGE.API_NOT_INITIALIZED}, 500

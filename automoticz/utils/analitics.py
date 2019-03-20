bef971132aa20a41e5bd0131b4660e6dd8cf2aca
xbound <nazarusko@gmail.com>
2019-03-20 15:56:27 +0100

    Work continues...

--- automoticz/utils/analitics.py
+++ automoticz/utils/analitics.py
@@ -5,8 +5,11 @@ from automoticz.domoticz.constants import Values
 
 
 def get_conditions_history(idx, time_range=Values.LOGS_RANGE_DAY):
-    '''
-    Get DateFrame with history from given device.
+    '''Get DateFrame with history from given device.
+
+    :param idx: idx of device.
+    :param time_range: time range from domoticz.constants.Values enum.
+    :return: DataFrame object
     '''
     records = get_temperature_history(idx, time_range)['result']
     raw_df = pd.DataFrame(records)
@@ -20,3 +23,28 @@ def get_conditions_history(idx, time_range=Values.LOGS_RANGE_DAY):
     df['temperature'] = df['te'].astype('float')
     df.drop(['hu', 'te'], axis=1, inplace=True)
     return df.set_index('d')
+
+
+def prepare_conditions_history(df: pd.DataFrame) -> pd.DataFrame:
+    '''Formats values in columns for existing DataFrame.
+
+    :param df: dataframe.
+    :return: DataFrame object.
+    '''
+    df['d'] = pd.to_datetime(df['d'])
+    df['date'] = df['d'].apply(lambda x: x.date())
+    df['time'] = df['d'].apply(lambda x: x.time())
+    df['weekday'] = df['d'].apply(lambda x: x.weekday())
+    df['workday'] = df['weekday'].apply(lambda x: 0 <= x <= 4)
+    df['humidity'] = df['humidity'].astype('float')
+    df['temperature'] = df['temperature'].astype('float')
+    return df.set_index('d')
+
+
+def make_room_conditions_data(room_df: pd.DataFrame, out_df) -> pd.DataFrame:
+    '''
+    Creates training DataFrame
+
+    :param room_df: dataframe with room's condition history.
+    :param out_df: dataframe with ouside condition hsitory. 
+    '''
